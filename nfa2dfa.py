30dc6aa58d2d6835deb1e540d95d9a0bba225f52
Tim Clark <tim.clark@propylon.com>
2015-10-02 13:50:39 -0500

    moved some functions into FA class. also pep8

--- nfa2dfa.py
+++ nfa2dfa.py
@@ -5,15 +5,63 @@ NULL = 'E'
 
 
 class FiniteAutomaton:
-    def __init__(self, initial_state, transitions, final_states, alphabet):
-        # Transitions are represented by nested key value pairs: the first key is the
-        # start state, the second key is the symbol to follow, the value is a list of
-        # resulting states. For example, transitions[2]['a'] will be a list of
-        # integers representing the states reachable from state 2 on input 'a'.        
+    def __init__(self, initial_state_or_flo, transitions=None,
+                 final_states=None, alphabet=None):
+        """Initialize a finite automaton. If initial_state_or_flo is a
+        file-like object, read from the file, otherwise use the parameters.
+        """
+        if type(initial_state_or_flo) is file:
+            self.read_from_file(initial_state_or_flo)
+        else:
+            # Transitions are represented by nested key value pairs: the first
+            # key is the start state, the second key is the symbol to follow,
+            # the value is a list of resulting states. For example,
+            # transitions[2]['a'] will be a list of integers representing the
+            # states reachable from state 2 on input 'a'.
+            self.initial_state = initial_state_or_flo
+            self.transitions = transitions
+            self.final_states = final_states
+            self.alphabet = alphabet
+
+    def read_from_file(self, flo):
+        """Get input from a file object and construct a FiniteAutomaton
+        object.
+        """
+        # get initial states
+        match = re.match(r"Initial State\:\s*\{(.*)\}", flo.readline())
+        self.initial_state = int(match.groups()[0])
+
+        # get final states
+        match = re.match(r"Final States\:\s*\{(.*)\}", flo.readline())
+        self.final_states = [
+            int(state) for state in match.groups()[0].split(',')]
+
+        # get state count - we don't actually need this
+        match = re.match(r"Total States\:\s*(\d*)$", flo.readline())
+        num_states = int(match.groups()[0])
+
+        # get state names
+        match = re.match(r"State\s*(.*)\s*$", flo.readline())
+        symbol_names = [name.strip() for name in match.groups()[0].split()]
+
+        # get transitions
+        state_pattern = r"(\d*)\s*" + r"\s*".join(
+            r"\{(.*)\}" for _ in symbol_names)
+        reo = re.compile(state_pattern)
+        transitions = {}
+        for state_string in flo.readlines():
+            groups = reo.match(state_string).groups()
+            from_state = int(groups[0])
+            end_state_strings = groups[1:]
+            transitions[from_state] = {}
+            for symbol, end_states in zip(symbol_names, end_state_strings):
+                if end_states:
+                    transitions[from_state][symbol] = [
+                        int(state) for state in end_states.split(",")]
         self.transitions = transitions
-        self.initial_state = initial_state
-        self.final_states = final_states
-        self.alphabet = alphabet
+
+        symbol_names.remove(NULL)  # get alphabet by removing null symbol
+        self.alphabet = symbol_names
 
     def null_closure(self, states):
         """Get states reachable from states along NULL transition"""
@@ -54,6 +102,51 @@ class FiniteAutomaton:
         if end not in self.transitions:
             self.transitions[end] = dict()
 
+    def to_dfa(self):
+        """Return equivalent dfa object"""
+        # create empty automaton to build from
+        dfa = FiniteAutomaton(1, dict(), [], self.alphabet)
+
+        initial_state = self.null_closure([self.initial_state])
+        print "E-closure(IO) = {{{}}} = 1\n".format(
+            ",".join(map(str, initial_state)))
+        dfa_states = {tuple(initial_state): 1}
+        # Rather than "marking" states, keep them in a queue
+        unmarked = [initial_state]
+
+        # Continue while there are "unmarked" states
+        while(unmarked):
+            start = unmarked.pop(0)
+            initial_state_name = dfa_states[tuple(start)]
+            print "Mark {}".format(initial_state_name)
+            for symbol in self.alphabet:
+                move = self.move(start, symbol)
+                if move:
+                    print_transition(start, symbol, move)
+                    new_state = self.null_closure(move)
+                    if tuple(new_state) not in dfa_states:
+                        # this set of nfa states is a new dfa state
+                        new_state_name = len(dfa_states) + 1
+                        dfa_states[tuple(new_state)] = new_state_name
+                        unmarked.append(tuple(new_state))
+                    else:
+                        # This state already exists
+                        new_state_name = dfa_states[tuple(new_state)]
+                    print_null_closure(move, new_state, new_state_name)
+                    # Add transition to dfa
+                    dfa.add_transition(
+                        initial_state_name, symbol, new_state_name)
+            print
+
+        # Set final states
+        self_final_states = set(self.final_states)
+        for state_set, state_name in dfa_states.items():
+            if self_final_states.intersection(state_set):
+                dfa.final_states.append(state_name)
+        print "dfa final states = {}".format(dfa.final_states)
+
+        return dfa
+
     def __str__(self):
         """How to print a dfa"""
         # Build the string line by line. Join at the end.
@@ -62,7 +155,7 @@ class FiniteAutomaton:
         lines.append(
             "Final States: {{{}}}".format(
                 ",".join(map(str, self.final_states))))
-        
+
         # column headers
         lines.append(
             "State\t{}".format("\t".join(self.alphabet)))
@@ -79,52 +172,6 @@ class FiniteAutomaton:
         return "\n".join(lines)
 
 
-def nfa_to_dfa(nfa):
-    """Take FiniteAutomaton object as input, return equivalent dfa object"""
-    # create empty automaton to build from
-    alphabet = nfa.alphabet
-    dfa = FiniteAutomaton(1, dict(), [], alphabet)
-
-    initial_state = nfa.null_closure([nfa.initial_state])
-    print "E-closure(IO) = {{{}}} = 1\n".format(
-        ",".join(map(str, initial_state)))
-    dfa_states = {tuple(initial_state): 1}
-    # Rather than "marking" states, keep them in a queue
-    unmarked = [initial_state]
-
-    # Continue while there are "unmarked" states
-    while(unmarked):
-        start = unmarked.pop(0)
-        initial_state_name = dfa_states[tuple(start)]
-        print "Mark {}".format(initial_state_name)
-        for symbol in alphabet:
-            move = nfa.move(start, symbol)
-            if move:
-                print_transition(start, symbol, move)
-                new_state = nfa.null_closure(move)
-                if tuple(new_state) not in dfa_states:
-                    # this set of nfa states is a new dfa state
-                    new_state_name = len(dfa_states) + 1
-                    dfa_states[tuple(new_state)] = new_state_name
-                    unmarked.append(tuple(new_state))
-                else:
-                    # This state already exists
-                    new_state_name = dfa_states[tuple(new_state)]
-                print_null_closure(move, new_state, new_state_name)
-                # Add transition to dfa
-                dfa.add_transition(initial_state_name, symbol, new_state_name)
-        print
-
-    # Set final states
-    nfa_final_states = set(nfa.final_states)
-    for state_set, state_name in dfa_states.items():
-        if nfa_final_states.intersection(state_set):
-            dfa.final_states.append(state_name)
-    print "dfa final states = {}".format(dfa.final_states)
-
-    return dfa
-                                       
-                
 def print_transition(start, symbol, end):
     """print a transition from start state to end state, along input symbol
     """
@@ -143,46 +190,9 @@ def print_null_closure(state, closure, name):
         ",".join(map(str, state)), ",".join(map(str, closure)), name)
 
 
-def file_to_nfa(flo):
-    """Get input from a file object and construct a FiniteAutomaton object."""
-    # get initial states
-    match = re.match(r"Initial State\:\s*\{(.*)\}", flo.readline())
-    initial_state = int(match.groups()[0])
-
-    # get final states
-    match = re.match(r"Final States\:\s*\{(.*)\}", flo.readline())
-    final_states = [int(state) for state in match.groups()[0].split(',')]
-    # get state count
-    match = re.match(r"Total States\:\s*(\d*)$", flo.readline())
-    num_states = int(match.groups()[0])
-
-    # get state names
-    match = re.match(r"State\s*(.*)\s*$", flo.readline())
-    symbol_names = [name.strip() for name in match.groups()[0].split()]
-
-    # get transitions
-    state_pattern = r"(\d*)\s*" + r"\s*".join(r"\{(.*)\}" for _ in symbol_names)
-    reo = re.compile(state_pattern)
-    transitions = {}
-    for state_string in flo.readlines():
-        groups = reo.match(state_string).groups()
-        from_state = int(groups[0])
-        end_state_strings = groups[1:]
-        transitions[from_state] = {}
-        for symbol, end_states in zip(symbol_names, end_state_strings):
-            if end_states:
-                transitions[from_state][symbol] = [
-                    int(state) for state in end_states.split(",")]
-
-    # return nfa
-    symbol_names.remove(NULL) # get alphabet by removing null symbol
-    return FiniteAutomaton(
-        initial_state, transitions, final_states, symbol_names)
-
-
 def main():
-    nfa = file_to_nfa(stdin)
-    dfa = nfa_to_dfa(nfa)
+    nfa = FiniteAutomaton(stdin)
+    dfa = nfa.to_dfa()
     print dfa
 
 
